60f474645b58ef4892b6affc91b01c44182e26c3
canhlq-1051 <le.quang.canh@sun-asterisk.com>
2019-11-11 09:30:31 +0700

    fix webpacker on develoment env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -8,7 +8,6 @@ bundle exec rake db:migrate
 
 rm -f tmp/pids/server.pid
 
-yarn install
 bundle exec rails assets:precompile
 bundle exec rails server -b 0.0.0.0
 
